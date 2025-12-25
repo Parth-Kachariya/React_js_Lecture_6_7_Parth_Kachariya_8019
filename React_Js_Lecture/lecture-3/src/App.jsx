f8df8b3be56215addad0c35b01aae53173b6083f
parth-kachariya <parthkachariya668@gmail.com>
2025-12-25 17:06:13 +0530

    Update || 25-12

--- React_Js_Lecture/lecture-3/src/App.jsx
+++ React_Js_Lecture/lecture-3/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import Appes from "./components/Appes";
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-4xl bg-slate-800 text-white font-extrabold mb-2 p-3 flex justify-center hover:bg-slate-900">Vite + React + tailwind</h1>
+      <Navbar />
+      <h1 className=" mt-3 text-4xl bg-slate-800 text-white font-extrabold mb-2 p-3 flex justify-center hover:bg-slate-900">
+        Vite + React + tailwind
+      </h1>
       <div className="card bg-gray-600 mb-2 hover:bg-slate-800">
         <button
           className="bg-red-600 p-3 font-bold text-white rounded-full cursor-pointer hover:bg-red-800"
@@ -16,7 +22,13 @@ function App() {
         </button>
       </div>
 
-      <h1 className="bg-slate-800 text-2xl font-bold text-white container flex justify-center p-3 hover:bg-gray-600"> Hello World</h1>
+      <Appes />
+
+      <h1 className="bg-slate-800 text-2xl font-bold text-white container flex justify-center p-3 hover:bg-gray-600">
+        {" "}
+        Hello World
+      </h1>
+      <Form />
     </>
   );
 }
---
+++ React_Js_Lecture/lecture-3/src/components/Appes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Appes = () => {
+  return (
+    <div>
+      <h1 className="bg-indigo-600 p-4 text-2xl font-bold  text-white mb-2 hover:bg-indigo-700">hello my world</h1>
+    </div>
+  );
+};
+
+export default Appes;
+
+
