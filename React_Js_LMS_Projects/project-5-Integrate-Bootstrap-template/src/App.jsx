bd0af0cca9ca8d793ff11c58926ec0e7196cb4ef
parth-kachariya <parthkachariya668@gmail.com>
2026-01-26 09:22:04 +0530

    Update || 26-1

--- React_Js_LMS_Projects/project-5-Integrate-Bootstrap-template/src/App.jsx
+++ React_Js_LMS_Projects/project-5-Integrate-Bootstrap-template/src/App.jsx
@@ -1,11 +1,13 @@
 import Accordions from "./Components/Accordions"
 import Buttons from "./Components/Buttons"
+import Card from "./Components/Card"
 
 const App = () => {
   return (
     <div className="container mx-auto">
       <Buttons />
       <Accordions />
+      <Card />
     </div>
   )
 }
---
+++ React_Js_LMS_Projects/project-5-Integrate-Bootstrap-template/src/Components/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from 'react-bootstrap/esm/Button'
+
+const Card = () => {
+    return (
+        <div className="container my-5">
+            <div className="row g-4">
+                {["Ui / Ux Design", "Web Development", "Project Deployment"].map((item, index) => (
+                    <div className="col-md-4" key={index}>
+                        <div className="card shadow-lg cardh">
+                            <div className="card-body">
+                                <h5 className="card-title">{item}</h5>
+                                <p className="card-text">
+                                    Professional {item.toLowerCase()} services using modern tools.
+                                </p>
+                                <Button variant="primary">Learn More</Button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Card
