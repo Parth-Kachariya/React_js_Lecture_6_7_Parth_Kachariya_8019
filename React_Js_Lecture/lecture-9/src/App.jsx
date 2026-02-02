fcf1736dddcdcb10f3d687b28c64912ae76d05a2
parth-kachariya <parthkachariya668@gmail.com>
2026-02-02 10:01:19 +0530

    Update|| 02-02

---
+++ React_Js_Lecture/lecture-9/src/App.jsx
@@ -0,0 +1,10 @@
+import Form from "./components/Form";
+const App = () => {
+  return (
+    <div className="max-w-7xl p-4 rounded-xl border mx-auto text-center">
+      <Form />
+    </div>
+  );
+};
+
+export default App;
