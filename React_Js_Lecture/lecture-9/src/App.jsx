4b2e3ea0013929fc59a516769b4029b4ca29be6b
parth-kachariya <parthkachariya668@gmail.com>
2026-02-03 10:10:10 +0530

    Update || 03-02

--- React_Js_Lecture/lecture-9/src/App.jsx
+++ React_Js_Lecture/lecture-9/src/App.jsx
@@ -1,8 +1,10 @@
 import Form from "./components/Form";
 const App = () => {
   return (
-    <div className="max-w-7xl p-4 rounded-xl border mx-auto text-center">
-      <Form />
+    <div className="h-screen flex justify-center  bg-gray-800 text-center">
+      <div className="flex justify-center items-center">
+        <Form />
+      </div>
     </div>
   );
 };
